7418d53629eb6fb03a979347349db301babff1ad
buddhiranasinghe <buddhiranasinghe@yahoo.com>
2020-08-04 09:03:50 -0700

    Dojo survey and Random number

--- django/django_intro/dojo_survey/survey_app/urls.py
+++ django/django_intro/dojo_survey/survey_app/urls.py
@@ -4,4 +4,7 @@ urlpatterns = [
 	path('', views.index),
 	path('process', views.process),
 	path('result', views.result),
+	path('random_word', views.random_word),
+	path('generate', views.generate),
+	path('reset', views.reset),
 ]
--- django/django_intro/dojo_survey/survey_app/views.py
+++ django/django_intro/dojo_survey/survey_app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render, redirect
+from django.utils.crypto import get_random_string
 
+# Create your views here.
 def index(request):
 	return render(request, 'form.html')
+
+# def process(request):
+# 	if request.method == 'POST':
+# 		print(request.POST)
+# 		context = {
+# 			'name': request.POST['user_name'],
+# 			'location': request.POST['location'],
+# 			'language': request.POST['fav_lang'],
+# 			'gender': request.POST['gender'],
+# 			'duration': request.POST['duration'],
+# 			'comment': request.POST['comment']
+# 		}
+# 		return render(request, 'result.html', context)
+# 	return render(request, 'result.html')
+
 
 def process(request):
 	print(request.POST)
@@ -9,11 +26,43 @@ def process(request):
 	request.session['location'] = request.POST['location']
 	request.session['language'] = request.POST['fav_lang']
 	request.session['gender'] = request.POST['gender']
-	# request.session['duration'] = request.POST['duration']
+	request.session['duration'] = request.POST['duration']
 	request.session['comment'] = request.POST['comment']
 	return redirect('/result')
 
 def result(request):
 	return render(request, "result.html")
 
-# Create your views here.
+def random_word(request):
+	if 'attempt_count' not in request.session:
+		request.session['attempt_count'] = 0
+	request.session['attempt_count'] += 1
+	request.session['random_word'] = get_random_string(length=14)
+	return render(request, "random_word.html")
+
+def generate(request):
+	request.session['attempt_count'] += 1
+	request.session['random_word'] = get_random_string(length=14)
+	return render(request, "random_word.html")
+
+def reset(request):
+	request.session.flush()
+	return redirect('/random_word')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
